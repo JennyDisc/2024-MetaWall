2d9c343cbfbc77878454798f9c216c5b167f17b0
JennyDisc <touki741@hotmail.com>
2024-05-27 18:01:06 +0800

    feat:增加 eslint 與 prettier 設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  // 全局引入 main.css 文件
+  css: ['~/assets/css/main.css']
+});
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    // './composables/**/*.{js,ts}',
+    './plugins/**/*.{js,ts}',
+    // './utils/**/*.{js,ts}',
+    './{App,app}.{js,ts,vue}',
+    // './{Error,error}.{js,ts,vue}',
+    // './app.config.{js,ts}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'gray1': '#EFECE7',
+        'gray2': '#00040029',
+        'gray3': '#707070',
+        'gray4': '#9B9893',
+        'black1': '#000400',
+        'blue1': '#03438D'
+      }
+    },
+  },
+  plugins: [],
+}
+
